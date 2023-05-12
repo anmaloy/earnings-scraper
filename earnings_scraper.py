128f161204f06b4fb66ea590776bb0fdc40a20f8
anmaloy <70681666+anmaloy@users.noreply.github.com>
2023-05-12 13:57:19 -0600

    Update earnings_scraper.py
    Allows user input directly.

--- earnings_scraper.py
+++ earnings_scraper.py
@@ -1,6 +1,4 @@
 from bs4 import BeautifulSoup
-import glob
-import os
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -8,46 +6,33 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-print('Earnings Download')
+ticker = input('Ticker: ')
+print(f'Downloading {ticker}')
 
-path = "data\\head\\*.csv"
+# Loads the webdriver
+options = Options()
+options.add_argument('--headless')
+page = f"https://www.zacks.com/stock/research/{ticker}/earnings-calendar"
+driver = webdriver.Chrome(options=options)
+driver.get(page)
 
-for file in glob.glob(path):
-    tickerList = pd.read_csv(file)
-    for i in range(len(tickerList)):
-        t = tickerList.iloc[i, 0]
-        head, tail = os.path.split(file)
-        name = os.path.splitext(tail)[0]
-        print('Downloading ' + t + ' from ' + name)
+# Selects from the dropdown, maximum of 100, or 25 years
+try:
+    x = driver.find_element('name', 'earnings_announcements_earnings_table_length')
+    drop = Select(x)
+    drop.select_by_visible_text("100")
+    time.sleep(2)
+except NoSuchElementException:
+    print('\tTable not found: skipping')
+    time.sleep(2)
 
-        # Loads the webdriver
-        options = Options()
-        options.add_argument('--headless')
-        page = f"https://www.zacks.com/stock/research/{t}/earnings-calendar"
-        driver = webdriver.Chrome(options=options)
-        driver.minimize_window()
-        driver.get(page)
+# Extracts the html
+soup = BeautifulSoup(driver.page_source, 'lxml')
+driver.close()
 
-        # Selects from the dropdown, maximum of 100, or 25 years
-        try:
-            x = driver.find_element('name', 'earnings_announcements_earnings_table_length')
-            drop = Select(x)
-            drop.select_by_visible_text("100")
-            time.sleep(2)
-        except NoSuchElementException:
-            print('\tTable not found: skipping')
-            tickerList = tickerList.drop([i])
-            time.sleep(2)
-            continue
+# Saves the table as a dataframe
+table = pd.read_html(str(soup.find(id='earnings_announcements_earnings_table')))[0]
 
-        # Extracts the html
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        driver.close()
-
-        # Saves the table as a dataframe
-        table = pd.read_html(str(soup.find(id='earnings_announcements_earnings_table')))[0]
-
-        # Exports dataframe to csv
-        table.to_csv(f'data\\earnings\\{name}\\{t}.csv', index=False)
-        print('\tDownloaded')
-    tickerList.to_csv(file, index=False)
+# Exports dataframe to csv
+table.to_csv(f'{ticker}_earnings.csv', index=False)
+print('\tDownloaded')
